a34361b4c9a38c991a3982f7843ce5b280df774c
santhitak <63070175@kmitl.ac.th>
2023-02-18 17:00:03 +0700

    initialize proj

---
+++ go.mod
@@ -0,0 +1,1 @@
+module co-working-space
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./apps/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
