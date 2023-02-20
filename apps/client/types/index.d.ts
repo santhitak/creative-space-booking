ba1103af1e0227abb2ff3c83f30c9c8cbbcb74fa
santhitak <63070175@kmitl.ac.th>
2023-02-21 00:07:19 +0700

    feat: basic types

---
+++ apps/client/types/index.d.ts
@@ -0,0 +1,35 @@
+export type User = {
+  userId?: string;
+  email?: string;
+  firstName?: string;
+  lastName?: string;
+  role: Role;
+};
+
+enum Role {
+  'admin',
+  'user',
+}
+
+export type OAuthUser = {
+  sub: string;
+  name: string;
+  given_name: string;
+  family_name: string;
+  picture: string;
+  email: string;
+  email_verified: boolean;
+  locale: string;
+};
+
+export type Room = {
+  title: string;
+  booking: Booking;
+};
+
+export type Booking = {
+  userId: string;
+  date: string;
+  startTime: string;
+  endTime: string;
+};
