0869a13924b319b345f3c6219dc3eb3207bd0c24
nata-non <63070045@kmitl.ac.th>
2023-02-21 22:49:31 +0700

    feat: basic Go api

--- apps/server/main.go
+++ apps/server/main.go
@@ -1,60 +1,55 @@
 package main
 
 import (
-	"context"
-	"errors"
 	"fmt"
-	"io"
-	"net"
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
-const keyServerAddr = "serverAddr"
-
 func check(e error) {
-  if e != nil {
-      panic(e)
-  }
+	if e != nil {
+		panic(e)
+	}
 }
-
-func getRoot(w http.ResponseWriter, r *http.Request) {
-  ctx := r.Context()
-
-	fmt.Printf("%s: got a request\n", ctx.Value(keyServerAddr))
-	io.WriteString(w, "Server is running!\n")
-}
-
 func main() {
-    fmt.Println("Hello, World!")
-    dat, err := os.ReadFile("./src/cow.txt")
-    check(err)
-    fmt.Print(string(dat))
-
-    mux := http.NewServeMux()
-    mux.HandleFunc("/", getRoot)
-
-    ctx, cancelCtx := context.WithCancel(context.Background())
-    serverOne := &http.Server{
-      Addr:    ":3000",
-      Handler: mux,
-      BaseContext: func(l net.Listener) context.Context {
-        ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
-        return ctx
-      },
-    }
-
-    fmt.Println("\nListening at port http://localhost:3000/")
-
-    go func() {
-		err := serverOne.ListenAndServe()
-		if errors.Is(err, http.ErrServerClosed) {
-			fmt.Printf("server closed\n")
-		} else if err != nil {
-			fmt.Printf("error listening for server one: %s\n", err)
+	type Hello struct {
+		Fname string `json:"fname"`
+		Lname string `json:"lname"`
+		Phone string `json:"phone"`
+	}
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dat, err := os.ReadFile("./src/cow.txt")
+	check(err)
+	fmt.Print(string(dat))
+	fmt.Println("start at port", os.Getenv("PORT"))
+	app := fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("hello")
+	})
+	app.Post("/user", func(ctx *fiber.Ctx) error {
+		// a := new(User)
+		p := struct {
+			Fname string
+			Lname string
+			Phone string
+		}{}
+		if err := ctx.BodyParser(&p); err != nil {
+			return err
 		}
-		cancelCtx()
-	  }()
-
-    <-ctx.Done()
+		a := Hello{
+			Fname: p.Fname,
+			Lname: p.Lname,
+			Phone: p.Phone,
+		}
+		return ctx.Status(http.StatusOK).JSON(a)
+	})
+	log.Fatal(app.Listen(":" + os.Getenv("PORT")))
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,44 @@
 module co-working-space
+
+go 1.19
+
+require github.com/gofiber/fiber/v2 v2.42.0
+
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.3.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	gorm.io/gorm v1.24.2 // indirect
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/joho/godotenv v1.5.1
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/lib/pq v1.10.7
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/philhofer/fwd v1.1.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/savsgio/dictpool v0.0.0-20221023140959-7bf2e61cea94 // indirect
+	github.com/savsgio/gotils v0.0.0-20220530130905-52f3993e8d6d // indirect
+	github.com/tinylib/msgp v1.1.6 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.44.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	github.com/yuin/goldmark v1.4.13 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
+	golang.org/x/net v0.6.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/term v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/tools v0.1.12 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gorm.io/driver/postgres v1.4.8
+)
