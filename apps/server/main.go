5b81ea0737a50c6c50a278f66f324a03b6aa93e8
santhitak <63070175@kmitl.ac.th>
2023-02-21 23:39:35 +0700

    feat: link in terminal

--- apps/server/main.go
+++ apps/server/main.go
@@ -16,40 +16,48 @@ func check(e error) {
 		panic(e)
 	}
 }
+
 func main() {
 	type Hello struct {
-		Fname string `json:"fname"`
-		Lname string `json:"lname"`
+		FirstName string `json:"firstName"`
+		LastName string `json:"lastName"`
 		Phone string `json:"phone"`
 	}
+
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
 	dat, err := os.ReadFile("./src/cow.txt")
 	check(err)
+
 	fmt.Print(string(dat))
-	fmt.Println("start at port", os.Getenv("PORT"))
+	fmt.Printf("\nlistening at http://localhost:%s", os.Getenv("PORT"))
 	app := fiber.New()
+
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("hello")
+		return c.SendString("Server is running!")
 	})
+
 	app.Post("/user", func(ctx *fiber.Ctx) error {
-		// a := new(User)
 		p := struct {
-			Fname string
-			Lname string
+			FirstName string
+			LastName string
 			Phone string
 		}{}
+
 		if err := ctx.BodyParser(&p); err != nil {
 			return err
 		}
+
 		a := Hello{
-			Fname: p.Fname,
-			Lname: p.Lname,
+			FirstName: p.FirstName,
+			LastName: p.LastName,
 			Phone: p.Phone,
 		}
 		return ctx.Status(http.StatusOK).JSON(a)
 	})
+
 	log.Fatal(app.Listen(":" + os.Getenv("PORT")))
 }
