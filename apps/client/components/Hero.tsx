fd50a3744fc2747e5c0660e2b5cab1d3c6c2fbfa
santhitak <63070175@kmitl.ac.th>
2023-03-06 13:23:08 +0700

    feat: applied grid layout

--- apps/client/components/Hero.tsx
+++ apps/client/components/Hero.tsx
@@ -1,22 +1,25 @@
 import Image from 'next/image';
+import GridLayout from './shared/GridLayout';
 
 const Hero = () => {
   return (
     <>
-      <div className="relative w-screen h-[480px]">
+      <div className="relative h-[480px]">
         <Image fill src="/images/bg.png" alt="" />
         <div className="absolute top-0 h-full flex items-center">
-          <div className="px-24">
-            <h2 className="font-semibold text-[#f3f3f3] text-7xl">
-              Co-Working Space
-            </h2>
-            <h2 className="font-semibold text-[#f3f3f3] text-7xl">
-              Creative Room Booking System
-            </h2>
-            <p className="text-[#f3f3f3] mt-8">
-              Booking with CORB today for a better living.
-            </p>
-          </div>
+          <GridLayout>
+            <>
+              <h2 className="font-semibold text-[#f3f3f3] text-7xl">
+                Co-Working Space
+              </h2>
+              <h2 className="font-semibold text-[#f3f3f3] text-7xl">
+                Creative Room Booking System
+              </h2>
+              <p className="text-[#f3f3f3] mt-8">
+                Booking with CORB today for a better living.
+              </p>
+            </>
+          </GridLayout>
         </div>
       </div>
     </>
--- apps/client/components/shared/Navbar.tsx
+++ apps/client/components/shared/Navbar.tsx
@@ -1,17 +1,24 @@
 import BlackButton from './BlackButton';
 import LightButton from './LightButton';
 import Image from 'next/image';
+import GridLayout from './GridLayout';
 
 const NavBar = () => {
   return (
     <div className="w-screen h-18 sticky top-0 bg-[#f3f3f3] z-40 shadow-md">
-      <div className="px-24 py-4 flex justify-between">
-        <Image width={220} height={44} src="/images/logo.png" alt="" />
-        <div className="flex space-x-4">
-          <BlackButton size={14} href="/auth/sign-in" text="Room Observation" />
-          <LightButton size={14} href="/auth/sign-in" text="Sign in" />
+      <GridLayout>
+        <div className="py-4 flex justify-between">
+          <Image width={220} height={44} src="/images/logo.png" alt="" />
+          <div className="flex space-x-4">
+            <BlackButton
+              size={14}
+              href="/auth/sign-in"
+              text="Room Observation"
+            />
+            <LightButton size={14} href="/auth/sign-in" text="Sign in" />
+          </div>
         </div>
-      </div>
+      </GridLayout>
     </div>
   );
 };
