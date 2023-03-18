b476653d9e1579ed302c97169f84955a337d5661
santhitak <63070175@kmitl.ac.th>
2023-03-18 14:09:28 +0700

    fix: ui match

--- apps/client/components/shared/BlackButton.tsx
+++ apps/client/components/shared/BlackButton.tsx
@@ -11,7 +11,7 @@ const BlackButton = (props: Props) => {
       <button
         className={`px-4 lg:px-6 py-2 rounded-[20px] lg:text-[16px] text-sm text-[#f3f3f3] bg-[#1e1e1e] border border-[#f3f3f3]`}
       >
-        <p>{props.text}</p>
+        {props.text}
       </button>
     </Link>
   );
