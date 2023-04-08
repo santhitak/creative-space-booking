73d4d7aae6b4ab2d93fa2b1b7c253f96fb46cf82
santhitak <santhita.sk@gmail.com>
2023-04-09 01:59:07 +0700

    feat: sign in as a modal instead of page

--- apps/client/components/auth/index.ts
+++ apps/client/components/auth/index.ts
@@ -1,1 +1,2 @@
 export { default as SignInButton } from './SignInbutton';
+export { default as SignInModal } from './SignInModal';
--- apps/client/components/shared/Navbar.tsx
+++ apps/client/components/shared/Navbar.tsx
@@ -4,6 +4,8 @@ import Image from 'next/image';
 import GridLayout from './GridLayout';
 import Link from 'next/link';
 import { User } from '../../types';
+import { useState } from 'react';
+import { SignInModal } from '../auth';
 
 interface Props {
   children: React.ReactNode;
@@ -11,6 +13,8 @@ interface Props {
 }
 
 const Navbar = ({ children, user }: Props) => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div>
       <div className="w-full h-18 sticky top-0 bg-[#f3f3f3] z-40 shadow-md">
@@ -19,23 +23,27 @@ const Navbar = ({ children, user }: Props) => {
             <Link href="/">
               <Image width={220} height={44} src="/images/logo.png" alt="" />
             </Link>
-            <div className="hidden lg:flex space-x-4">
-              <BlackButton
-                href="/auth/sign-in"
-                text={<p>Room Observation</p>}
-              />
-              <LightButton href="/auth/sign-in" text="Sign in" />
-            </div>
-            <div className="lg:hidden">
+            <div>
               {!user ? (
-                <LightButton href="/auth/sign-in" text="Sign in" />
+                <LightButton
+                  href="#"
+                  action={() => setOpen(true)}
+                  text="Sign in"
+                />
               ) : (
-                <LightButton href="/auth/sign-in" text={user.firstName} />
+                <div className="flex space-x-4">
+                  <BlackButton
+                    href="/auth/sign-in"
+                    text={<p>Room Observation</p>}
+                  />
+                  <LightButton href="/auth/sign-in" text={user.firstName} />
+                </div>
               )}
             </div>
           </div>
         </GridLayout>
       </div>
+      <SignInModal open={open} setOpen={setOpen} />
       <div className="w-full">{children}</div>
     </div>
   );
