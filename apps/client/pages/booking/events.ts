135c49e40711cde398ae76b7ec957eb5e32233f2
Xlaperr <64070169@it.kmitl.ac.th>
2023-04-15 06:46:45 +0700

    perf: refactor components

--- apps/client/pages/booking/events.ts
+++ apps/client/pages/booking/events.ts
@@ -2,12 +2,12 @@ import { CalendarEvent } from 'types';
 
 const eventList: CalendarEvent[] = [
   {
-    title: 'Santhita',
+    title: 'Santhita (project)',
     start: new Date('2023-04-14T02:30:00.000Z'),
     end: new Date('2023-04-14T03:30:00.000Z'),
   },
   {
-    title: 'Natanon',
+    title: 'Natanon (meeting)',
     start: new Date('2023-04-14T04:30:00.000Z'),
     end: new Date('2023-04-14T05:30:00.000Z'),
   },
--- apps/client/pages/booking/index.tsx
+++ apps/client/pages/booking/index.tsx
@@ -1,11 +1,10 @@
-import { ScheduleView } from '@/components/booking';
+import { RoomListBox, ScheduleView } from '@/components/booking';
 import { GridLayout, Loading } from '@/components/shared';
 import { useEffect, useState } from 'react';
 import { Room } from 'types';
 
 const BookingPage = () => {
   const url = process.env.BACKEND_URL;
-  const [selected, setSelected] = useState(1);
   const [room, setRoom] = useState<Room[]>();
   const [roomData, setRoomData] = useState<Room>();
 
@@ -32,26 +31,13 @@ const BookingPage = () => {
           )}
           {room && (
             <div className="flex flex-col gap-y-6">
-              <div className="flex gap-x-4">
-                {room.map((item: Room) => (
-                  <button
-                    onClick={() => {
-                      setSelected(item.id);
-                      setRoomData(item);
-                    }}
-                    className={`${
-                      selected === item.id
-                        ? 'bg-blue-800 text-white'
-                        : 'border border-blue-800 text-blue-800'
-                    } rounded-md py-3 px-4 select-none`}
-                    key={item.id}
-                  >
-                    <p>{item.name}</p>
-                  </button>
-                ))}
-              </div>
               <div className="">
-                <ScheduleView room={roomData} />
+                <RoomListBox
+                  room={room}
+                  roomData={!roomData ? room[0] : roomData}
+                  setRoomData={setRoomData}
+                />
+                <ScheduleView room={!roomData ? room[0] : roomData} />
               </div>
             </div>
           )}
