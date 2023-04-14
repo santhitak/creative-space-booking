074d22552cb1c32f75a8d096167c89bd8e15790f
Xlaperr <64070169@it.kmitl.ac.th>
2023-04-14 16:28:35 +0700

    feat: mockup data

--- apps/client/pages/booking/events.ts
+++ apps/client/pages/booking/events.ts
@@ -2,12 +2,12 @@ import { CalendarEvent } from 'types';
 
 const eventList: CalendarEvent[] = [
   {
-    title: 'All Day Event very long title',
+    title: 'Santhita',
     start: new Date('2023-04-14T02:30:00.000Z'),
     end: new Date('2023-04-14T03:30:00.000Z'),
   },
   {
-    title: 'long title',
+    title: 'Natanon',
     start: new Date('2023-04-14T04:30:00.000Z'),
     end: new Date('2023-04-14T05:30:00.000Z'),
   },
