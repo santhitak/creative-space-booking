3df1a42af9b28d4fd52bed27c8746093f74a0b22
Xlaperr <64070169@it.kmitl.ac.th>
2023-04-15 06:46:07 +0700

    feat: change components layout

---
+++ apps/client/components/booking/RoomListBox.tsx
@@ -0,0 +1,70 @@
+import { Fragment } from 'react';
+import { Listbox, Transition } from '@headlessui/react';
+import { HiChevronUpDown } from 'react-icons/hi2';
+import { BsCheck2 } from 'react-icons/bs';
+import { Room } from 'types';
+
+interface Props {
+  room: Room[];
+  roomData: Room;
+  setRoomData: (roomData: Room) => void;
+}
+
+export default function RoomListBox(props: Props) {
+  const { room, setRoomData, roomData } = props;
+
+  return (
+    <div className="pb-10 w-full">
+      <Listbox value={roomData} onChange={setRoomData}>
+        <div className="relative mt-1 z-10">
+          <Listbox.Button className="relative w-full cursor-default rounded-lg bg-gray-200/50 border border-gray-300/60 py-2 pl-3 pr-10 text-left shadow-sm focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-blue-300 sm:text-sm">
+            <span className="block truncate">{roomData?.name}</span>
+            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+              <HiChevronUpDown
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </span>
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {room.map((item: Room) => (
+                <Listbox.Option
+                  key={item.id}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                      active ? 'bg-blue-100 text-blue-900' : 'text-gray-900'
+                    }`
+                  }
+                  value={item}
+                >
+                  {({ selected }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? 'font-medium' : 'font-normal'
+                        }`}
+                      >
+                        {item.name}
+                      </span>
+                      {selected ? (
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-blue-600">
+                          <BsCheck2 className="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
+  );
+}
--- apps/client/components/booking/ScheduleView.tsx
+++ apps/client/components/booking/ScheduleView.tsx
@@ -3,6 +3,8 @@ import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import eventList from 'pages/booking/events';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { useState } from 'react';
+import BookingModal from './BookingModal';
 const localizer = momentLocalizer(moment);
 
 interface Props {
@@ -12,27 +14,52 @@ interface Props {
 const ScheduleView = (props: Props) => {
   const { room } = props;
   const date = new Date();
+  const [openBooking, setOpenBooking] = useState(false);
+  const [currentDateShow, setCurrentDateShow] = useState(false);
+  const dateMoment = {
+    todayMoment: moment(),
+    tomorrowMoment: moment().clone().add(1, 'days'),
+    today: moment().format('LL'),
+    tomorrow: moment().clone().add(1, 'days').format('LL'),
+  };
 
   return (
     <div className="w-full">
       <div className="border border-gray-200 rounded-md">
-        <div className=" bg-gray-100/80 px-6 py-4 rounded-t-md flex justify-between">
-          <div className="">
-            <p className="font-semibold">
-              {date.toLocaleDateString('en-th', {
-                month: 'long',
-                day: 'numeric',
-                year: 'numeric',
-              })}
-            </p>
-            {date.toLocaleDateString('en-th', { weekday: 'long' })}
+        <div className=" bg-gray-100/80 px-6 py-4 rounded-t-md justify-between grid gap-2 md:flex">
+          <div className="space-x-2 items-center divide-x gap-x-2 flex">
+            <div>
+              <p className="font-semibold">
+                {!currentDateShow ? dateMoment.today : dateMoment.tomorrow}
+              </p>
+              <p>
+                {!currentDateShow
+                  ? date.toLocaleDateString('en-th', { weekday: 'long' })
+                  : ''}
+              </p>
+            </div>
+            <div className="pl-4">
+              <button
+                className="border bg-gray-300 py-2 text-gray-600 font-semibold px-4 rounded-md"
+                onClick={() => {
+                  !currentDateShow
+                    ? setCurrentDateShow(true)
+                    : setCurrentDateShow(false);
+                }}
+              >
+                {!currentDateShow ? 'Next' : 'Prev'}
+              </button>
+            </div>
           </div>
           <div className="flex items-center divide-x gap-x-4">
-            <p className="font-medium">
-              {room?.name ? room.name : 'Peer Tutor Room 1'}
-            </p>
+            <p className="font-medium">{room?.name}</p>
             <div className="pl-4">
-              <button className="bg-blue-700 px-4 py-2 text-white rounded-md">
+              <button
+                className="bg-blue-700 px-4 py-2 text-white rounded-md"
+                onClick={() => {
+                  setOpenBooking(true);
+                }}
+              >
                 Add booking
               </button>
             </div>
@@ -46,13 +73,17 @@ const ScheduleView = (props: Props) => {
               defaultView="day"
               startAccessor="start"
               endAccessor="end"
-              defaultDate={moment().toDate()}
+              min={moment('9:00', 'h:mm').toDate()}
+              max={moment('21:00', 'h:mm').toDate()}
+              defaultDate={dateMoment.today}
+              step={15}
               style={{
                 width: '100%',
               }}
             />
           </div>
         </div>
+        <BookingModal open={openBooking} setOpen={setOpenBooking} room={room} />
       </div>
     </div>
   );
