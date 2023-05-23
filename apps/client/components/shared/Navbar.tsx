6089b58f87555182db1ce54967b58249d362c87b
santhitak <santhita.sk@gmail.com>
2023-05-24 02:00:24 +0700

    feat: change navbar style

--- apps/client/components/shared/Navbar.tsx
+++ apps/client/components/shared/Navbar.tsx
@@ -1,12 +1,13 @@
-import BlackButton from './BlackButton';
-import LightButton from './LightButton';
+import { usePathname } from 'next/navigation';
 import Image from 'next/image';
 import GridLayout from './GridLayout';
 import Link from 'next/link';
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 import { SignInModal } from '../auth';
 import { useRouter } from 'next/router';
 import { User } from 'types';
+import { Menu, Transition } from '@headlessui/react';
+import { RiArrowDownSLine } from 'react-icons/ri';
 
 interface Props {
   children: React.ReactNode;
@@ -17,6 +18,17 @@ const Navbar = ({ children, userData }: Props) => {
   const [open, setOpen] = useState(false);
   const [user] = useState<User>(userData);
   const router = useRouter();
+  const pathname = usePathname();
+
+  function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ');
+  }
+
+  const navigation = [
+    { name: 'Home', href: '/' },
+    { name: 'Room Observation', href: '/booking' },
+    { name: 'History', href: '/booking-history' },
+  ];
 
   const handleSignOut = async () => {
     await fetch('/api/signout', {
@@ -39,20 +51,72 @@ const Navbar = ({ children, userData }: Props) => {
                 alt=""
               />
             </Link>
-            <div>
-              {!user ? (
-                <LightButton action={() => setOpen(true)} text="Sign in" />
-              ) : (
-                <div className="flex space-x-4">
-                  <BlackButton href="/booking" text={<p>Room Observation</p>} />
-                  <LightButton text={user.studentId} />
-                  <LightButton
-                    text={'Sign out'}
-                    action={() => handleSignOut()}
-                  />
-                </div>
+            <Menu as="div" className="relative inline-block">
+              <div>
+                <Menu.Button className="inline-flex w-full justify-center rounded-md px-4 py-2 text-sm font-medium text-[#1d1d1d] hover:bg-opacity-90 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 gap-2">
+                  {user ? (
+                    <div className="px-4 py-2 bg-[#1d1d1d] rounded-md text-[#f1f1f1] flex items-center gap-1">
+                      <p>{user.studentId}</p>
+                      <RiArrowDownSLine className="w-5 h-5" />
+                    </div>
+                  ) : (
+                    <div
+                      className="px-6 py-2 bg-[#1d1d1d] rounded-md text-[#f1f1f1]"
+                      onClick={() => setOpen(true)}
+                    >
+                      <p>Sign in</p>
+                    </div>
+                  )}
+                </Menu.Button>
+              </div>
+              {user && (
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-200"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none divide-y divide-gray-100 ">
+                    <div>
+                      {navigation.map((item, i) => (
+                        <Menu.Item key={i}>
+                          {({ active }) => (
+                            <Link
+                              href={item.href}
+                              className={classNames(
+                                pathname === item.href ? 'bg-gray-100' : '',
+                                'flex w-full px-4 py-2 text-sm text-gray-700'
+                              )}
+                              aria-current={
+                                pathname === item.href ? 'page' : undefined
+                              }
+                            >
+                              {item.name}
+                            </Link>
+                          )}
+                        </Menu.Item>
+                      ))}
+                    </div>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <button
+                          className={classNames(
+                            active ? 'bg-gray-100' : '',
+                            'flex w-full px-4 py-2 text-sm text-gray-700'
+                          )}
+                          onClick={handleSignOut}
+                        >
+                          Sign out
+                        </button>
+                      )}
+                    </Menu.Item>
+                  </Menu.Items>
+                </Transition>
               )}
-            </div>
+            </Menu>
           </div>
         </GridLayout>
       </div>
